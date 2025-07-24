4236265036e6ce6534f8668a55a433f5282c54d1
ronin <roc0ast3r@gmail.com>
2025-07-23 22:58:34 -0700

    potential firefox fix

--- dist/script.js
+++ dist/script.js
@@ -1180,18 +1180,6 @@ const progressBar = document.querySelector(".progress-bar");
 const endScreen = document.getElementById("end-screen");
 const endScreenScore = document.getElementById("end-score");
 let previousAlbums = [];
-// CODE BELOW IS FOR TESTING. COMMENT OUT WHEN DONE.
-// newRound()
-// home.classList.add("none")
-// game.classList.remove("none")
-// progressBar.classList.remove("none")
-// localStorage.setItem(ls.genre_no, "2")
-// localStorage.setItem(ls.max_rounds, "1")
-// localStorage.setItem(ls.pop_min, "99")
-// localStorage.setItem(ls.pop_max, "100")
-// localStorage.setItem(ls.hide_info, "false")
-// localStorage.setItem(ls.allow_explicit, "false")
-// CODE ABOVE IS FOR TESTING. COMMENT OUT WHEN DONE.
 let score = 0;
 let currentRound = 0;
 let maxRound = Number(localStorage.getItem(ls.max_rounds));
